81227beefce1e24a926491c1e0a93023ca53ab2a
Sandro Griebeler <sandrogriebeler@gmail.com>
2018-12-05 12:16:57 -0200

    added orders updateStatus + .env samples

--- samples/mock.js
+++ samples/mock.js
@@ -1,7 +1,10 @@
+require('dotenv').load();
+
 module.exports = {
     configure: {
-        mode: 'sandbox',
-        shopId: 'SHOP_ID',
-        token: 'TOKEN',
+        mode: process.env.APP_MODE,
+        url: process.env.APP_URL,
+        shopId: process.env.APP_SHOP_ID,
+        token: process.env.APP_TOKEN
     }
 };
--- samples/orders.js
+++ samples/orders.js
@@ -5,13 +5,20 @@ const mock = require('./mock');
 
     dooca.configure(mock.configure);
 
+    // get orders
     try {
-
         const getData = await dooca.orders.get({ limit: 2, offset: 0 });
         console.log(getData);
-
     } catch (e) {
-        console.log(e.toString());
+        console.log(e.response.data);
     }
+
+    // update order status
+    // try {
+    //     const response = await dooca.orders.updateStatus(651, 'approved');
+    //     console.log(response);
+    // } catch (e) {
+    //     console.log(e.response.data);
+    // }
 
 })();
